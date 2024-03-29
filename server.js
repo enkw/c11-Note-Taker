b33495df1848c94901991f9cb0d38305a079faa4
enkw <iankwolfe@gmail.com>
2024-03-28 21:08:40 -0400

    Started adding routes and what I hope saves notes to the db.json

--- server.js
+++ server.js
@@ -1,20 +1,53 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/assets/js/index.js');
-
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-
 app.use(express.static('public'));
 
+// Variable path to the database
+const notePath = path.resolve(__dirname, 'db', 'db.json');
+
+let notes = [];
+
+// Checks db.json for existing notes
+fs.readFile(notePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading notes from db.json', err);
+        return;
+    }
+    try {
+        notes = JSON.parse(data);
+    }
+    catch (error) {
+        console.error('Error parsing notes from db.json', error);
+    }
+});
+
+// Saves notes to db.json
+const saveNotes = () => {
+    fs.writeFile(notePath, JSON.stringify(notes), (err) => {
+        if (err) {
+            console.error('Error saving notes to db.json', err);
+        }
+    });
+};
+
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 );
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
